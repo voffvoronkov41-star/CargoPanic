fc9111a07bfc02e5b3e226160e515902f0bcc8fe
voffvoronkov41-star <voff.voronkov41@gmail.com>
2026-07-28 00:41:17 +0300

    Night Line: update www/sw.js

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'cargo-panic-v1';
-const FILES = ['./','index.html','style.css','game.js','manifest.webmanifest','assets/warehouse-bg.png','assets/worker-sheet.png','assets/crates-sheet.png','assets/icon-192.png','assets/icon-512.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = "night-line-v1";
+const FILES = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "assets/icon.svg", "assets/station-07.png"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
